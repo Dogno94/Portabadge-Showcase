a86eee11ab06b8b59e9c5eebcdd50b55d66f39f1
Dogno94 <dogno94@gmail.com>
2025-04-19 11:13:24 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
             <fog attach="fog" args={['#101010', 10, 20]} />
             <PerspectiveCamera
               makeDefault position={[0, 0, 12]}
-              fov={isMobile ? 60: 62.5}
+              fov={isMobile ? 63: 62.5}
               near={0.1}
               far={1000}
             />
